827edcf14d84c214d85af8b3526c85a98af7494e
KellyChen <matijueming@gmail.com>
2024-11-16 17:48:27 +0800

    Initial Commit

---
+++ SoloSetGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoloSetGame
+//
+//  Created by Jia Chen on 2024/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoloSetGame/SoloSetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloSetGameApp.swift
+//  SoloSetGame
+//
+//  Created by Jia Chen on 2024/11/16.
+//
+
+import SwiftUI
+
+@main
+struct SoloSetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
